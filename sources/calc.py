be169b9df16105312686187f4c5fead9e529eac5
xavier-1-tech <53019832+xavier-1-tech@users.noreply.github.com>
2021-08-14 16:10:15 -0400

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -17,12 +17,18 @@ def conv(value):
             return str(value)
 
 # The 'add2' function itself
-def add2(arg1, arg2):
+def add2(arg1, arg2, arg3, arg4, arg5):
     # Convert 'arg1' and 'arg2' to their appropriate types
     arg1conv = conv(arg1)
     arg2conv = conv(arg2)
+    arg3conv = conv(arg3)
+    arg4conv = conv(arg4)
+    arg5conv = conv(arg5)
     # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
-    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
+    if isinstance(arg1conv, str) or isinstance(arg2conv, str) or isinstance(arg3conv, str) or isinstance(arg4conv, str) or isinstance(arg5conv, str):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
-    return arg1conv + arg2conv
+        arg3conv = str(arg3conv)
+        arg4conv = str(arg4conv)
+        arg5conv = str(arg5conv)
+    return arg1conv + arg2conv + arg3conv + arg4conv + arg5conv
