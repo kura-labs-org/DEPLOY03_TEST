10913b9c2ba4e04afea01c7ac4c7a10853230157
SrKoDes <84875113+SrKoDes@users.noreply.github.com>
2021-08-14 16:06:44 -0400

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -26,3 +26,15 @@ def add2(arg1, arg2):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
     return arg1conv + arg2conv
+
+
+# The 'minus2' function itself
+def minus2(arg1, arg2):
+    # Convert 'arg1' and 'arg2' to their appropriate types
+    arg1conv = conv(arg1)
+    arg2conv = conv(arg2)
+    # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
+    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
+        arg1conv = str(arg1conv)
+        arg2conv = str(arg2conv)
+    return arg1conv - arg2conv
