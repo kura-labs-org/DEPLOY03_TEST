08aca3c4a5a158f5ca9b7787e17b4c884619c601
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-14 17:20:56 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -20,68 +20,6 @@ class TestCalc(unittest.TestCase):
         result = calc.add2('10.5', 2)
         self.assertEqual(result, 12.5)
 
-    def test_subtract_strings(self):
-        """
-        Test the addition of two strings returns the two strings as one
-        concatenated string
-        """
-        result = calc.2('abc', 'def')
-        self.assertEqual(result, 'abcdef')
-
-    def test_subtract_string_and_integer(self):
-        """
-        Test the addition of a string and an integer returns them as one
-        concatenated string (in which the integer is converted to a string)
-        """
-        result = calc.subtract2('abc', 3)
-        self.assertEqual(result, 'abc3')
-
-    def test_subtract_string_and_number(self):
-        """
-        Test the addition of a string and a float returns them as one
-        concatenated string (in which the float is converted to a string)
-        """
-        result = calc.subtract2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
- 
-    def test_subtract_integers(self):
-        """
-        Test that the addition of two integers returns the correct total
-        """
-        result = calc.subtract2(1, 2)
-        self.assertEqual(result, 3)
-
-    def test_subtract_floats(self):
-        """
-        Test that the addition of two floats returns the correct result
-        """
-        result = calc.subtract2('10.5', 2)
-        self.assertEqual(result, 12.5)
-
-    def test_subtract_strings(self):
-        """
-        Test the addition of two strings returns the two strings as one
-        concatenated string
-        """
-        result = calc.subtract2('abc', 'def')
-        self.assertEqual(result, 'abcdef')
-
-    def test_subtract_string_and_integer(self):
-        """
-        Test the addition of a string and an integer returns them as one
-        concatenated string (in which the integer is converted to a string)
-        """
-        result = calc.subtract2('abc', 3)
-        self.assertEqual(result, 'abc3')
-
-    def test_subtract_string_and_number(self):
-        """
-        Test the addition of a string and a float returns them as one
-        concatenated string (in which the float is converted to a string)
-        """
-        result = calc.subtract2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
-
 
 if __name__ == '__main__':
     unittest.main()
