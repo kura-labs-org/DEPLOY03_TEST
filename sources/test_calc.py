02b834afa56235c4c0da33f0c55ac36075d6ea9f
SrKoDes <84875113+SrKoDes@users.noreply.github.com>
2021-08-14 16:21:28 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -46,3 +46,50 @@ class TestCalc(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
+    
+class TestSecondCalc(unittest.TestCase):
+    """
+    Test the minus function from the calc library
+    """
+
+    def test_minus_integers(self):
+        """
+        Test that the subtraction of two integers returns the correct total
+        """
+        result = calc.minus2(1, 2)
+        self.assertEqual(result, 3)
+
+    def test_minus_floats(self):
+        """
+        Test that the subtractions of two floats returns the correct result
+        """
+        result = calc.minus2('10.5', 2)
+        self.assertEqual(result, 12.5)
+
+    def test_minus_strings(self):
+        """
+        Test the subtraction of two strings returns the two strings as one
+        concatenated string
+        """
+        result = calc.minus2('abc', 'def')
+        self.assertEqual(result, 'abcdef')
+
+    def test_minus_string_and_integer(self):
+        """
+        Test the subtraction of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string)
+        """
+        result = calc.minus2('abc', 3)
+        self.assertEqual(result, 'abc3')
+
+    def test_minus_string_and_number(self):
+        """
+        Test the subtraction of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string)
+        """
+        result = calc.minus2('abc', '5.5')
+        self.assertEqual(result, 'abc5.5')
+
+if __name__ == '__main__':
+    unittest.main()
