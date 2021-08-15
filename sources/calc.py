cc9fe75fc323249a509cad45e54236f5021399a8
Dilobar Irisova <84366385+DIrisova@users.noreply.github.com>
2021-08-15 12:06:20 -0400

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -26,3 +26,12 @@ def add2(arg1, arg2):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
     return arg1conv + arg2conv
+
+def subtract2(arg1, arg2):
+    arg1conv = conv(arg1)
+    arg2conv = conv(arg2)
+    
+    if isinstance(arg1conv,str) or isinstance(arg2conv, str):
+        arg1conv = str(arg1conv)
+        arg2conv = str(arg2conv)
+    return arg1conv - arg2conv
