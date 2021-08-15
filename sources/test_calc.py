fc8840400f4b8e5b1acaeaee8081d497fe72704d
ian-emagine <imitchell@emagine.lc>
2021-08-14 20:02:10 -0400

    Update to include muli2 tests

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -44,5 +44,48 @@ class TestCalc(unittest.TestCase):
         result = calc.add2('abc', '5.5')
         self.assertEqual(result, 'abc5.5')
 
+"""
+    Test the multiply function from the calc library
+    """
+
+    def test_multiply_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.multi2(1, 2)
+        self.assertEqual(result, 2)
+
+    def test_multiply_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = calc.add2('10.5', 2)
+        self.assertEqual(result, 21)
+
+    def test_multiply_strings(self):
+        """
+        Test the addition of two strings returns the two strings as one
+        concatenated string
+        """
+        result = calc.add2('abc', 'def')
+        self.assertEqual(result, 'abcdef')
+
+    def test_multiply_string_and_integer(self):
+        """
+        Test the addition of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string)
+        """
+        result = calc.add2('abc', 3)
+        self.assertEqual(result, 'abcabcabc')
+
+    def test_multiply_string_and_number(self):
+        """
+        Test the addition of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string)
+        """
+        result = calc.add2('abc', '5.5')
+        self.assertEqual(result, 'abcabcabcabcabc')
+
+        
 if __name__ == '__main__':
     unittest.main()
