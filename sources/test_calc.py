3508ebc507811a0764acedf020dc93b57c352800
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-15 12:48:03 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -10,31 +10,31 @@ class TestCalc(unittest.TestCase):
         """
         Test that the addition of two integers returns the correct total
         """
-        result = calc.add2(2, 3, 4)
-        self.assertEqual(result, 6)
+        result = calc.add2(2, 3)
+        self.assertEqual(result, 5)
 
     def test_add_floats(self):
         """
         Test that the addition of two floats returns the correct result
         """
-        result = calc.add2('10.5', 2, 3)
-        self.assertEqual(result, 15.5)
+        result = calc.add2('10.5', 2)
+        self.assertEqual(result, 12.5)
 
     def test_add_strings(self):
         """
         Test the addition of two strings returns the two strings as one
         concatenated string
         """
-        result = calc.add2('abc', 'def', 'abc')
-        self.assertEqual(result, 'abcdefabc')
+        result = calc.add2('abc', 'def')
+        self.assertEqual(result, 'abcdef')
 
     def test_add_string_and_integer(self):
         """
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string)
         """
-        result = calc.add2('abc', 3, 33)
-        self.assertEqual(result, 'abc333')
+        result = calc.add2('abc', 3)
+        self.assertEqual(result, 'abc3')
 
     
     def test_subtract_integers(self):
