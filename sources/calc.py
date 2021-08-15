c4099d6c69a3c3bbd8170ba4f669d40d8d62c0c3
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-15 12:32:25 -0400

    Update calc.py

--- sources/calc.py
+++ sources/calc.py
@@ -17,21 +17,25 @@ def conv(value):
             return str(value)
 
 # The 'add2' function itself
-def add2(arg1, arg2):
+def add2(arg1, arg2, arg3):
     # Convert 'arg1' and 'arg2' to their appropriate types
     arg1conv = conv(arg1)
     arg2conv = conv(arg2)
+    arg3conv = conv(arg3)
     # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
-    if isinstance(arg1conv, str) or isinstance(arg2conv, str):
+    if isinstance(arg1conv, str) or isinstance(arg2conv, str) or isinstance(arg3conv, str):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
-    return arg1conv + arg2conv
+        arg3conv = str(arg3conv)
+    return arg1conv + arg2conv + arg3conv
 
-def subtract2(arg1, arg2):
+def subtract2(arg1, arg2, arg3):
     arg1conv = conv(arg1)
     arg2conv = conv(arg2)
+    arg3conv = conv(arg3)
     
-    if isinstance(arg1conv,str) or isinstance(arg2conv, str):
+    if isinstance(arg1conv,str) or isinstance(arg2conv, str) or isinstance(arg3conv, str):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
-    return arg1conv - arg2conv
+        arg3conv = str(arg3conv)
+    return arg1conv - arg2conv - arg3conv
