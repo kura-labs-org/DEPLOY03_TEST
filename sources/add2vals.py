34ff97efd5065b9476756912af5d462ac559e482
Ricardo <49110228+Deodutt@users.noreply.github.com>
2021-08-15 20:20:14 -0400

    Update add2vals.py
    Updates the add 3 arguments feature.

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -14,11 +14,11 @@ if argnumbers == 2:
     print("")
     sys.exit(0)
 
-elif argnumbers != 2:
+elif argnumbers != 2 or argnumbers != 3:
     print("")
     print("You entered " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print("Usage: 'add2vals X Y (and/or Z)' where X and Y (and/or Z) are individual values.")
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
     print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
@@ -29,13 +29,3 @@ elif argnumbers == 3:
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))))
     print("")
     sys.exit(0)
-
-elif argnumbers != 3:
-    print("")
-    print("You entered " + str(argnumbers) + " value/s.")
-    print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
-    print("")
-    sys.exit(1)
