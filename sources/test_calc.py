b097487771942c7a7bbc763d43465a227e2f1939
Ricardo <49110228+Deodutt@users.noreply.github.com>
2021-08-15 20:51:43 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -59,12 +59,12 @@ class TestCalc(unittest.TestCase):
         result = calc.sub3('10.5', 2, 20)
         self.assertEqual(result, -11.5)
         
-       def test_add_strings(self):
+       def test_sub_strings(self):
         """
         Test the addition of two strings returns the two strings as one
         concatenated string
         """
-        result = calc.add2('abc', 'def', 'ghi')
+        result = calc.sub3('abc', 'def', 'ghi')
         self.assertEqual(result, 'abcdefghi')
 
 if __name__ == '__main__':
