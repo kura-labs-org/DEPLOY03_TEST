63d8c27ea432d81e9348339ee2a3e06245985ba1
SaiHoYip <78388916+SaiHoYip@users.noreply.github.com>
2021-08-15 20:21:50 -0400

    Update add2vals.py

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -14,11 +14,12 @@ if argnumbers == 3 :
     print("")
     sys.exit(0)
 
-elif argnumbers != 3 :
+elif argnumbers != 3 or argnumbers != 2:
     print("")
     print("You entered " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print("Usage: 'add2vals X Y' where X and Y are individual values. or 'subtract3vals' XYZ where xyz are individual values.")
+    
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
     print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
@@ -29,15 +30,5 @@ elif argnumbers == 2:
     print("The result is " + str(calc.subtract3(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
     sys.exit(0)
-
-elif argnumbers != 2:
-    print("")
-    print("You entered " + str(argnumbers) + " value/s.")
-    print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
-    print("")
-    sys.exit(1)
     
     
