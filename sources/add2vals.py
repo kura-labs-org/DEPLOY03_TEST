fdc4a225526bca7c942598a4855d4cd35d08112c
denzel-gustave <87156086+denzel-gustave@users.noreply.github.com>
2021-08-19 16:46:18 -0400

    Update Add4Vals
    Added new feature
    -Modified Add2Vals to Add4Vals

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -13,13 +13,19 @@ if argnumbers == 2 :
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
     sys.exit(0)
+    
+elif argnumbers == 4 :
+    print("")
+    print("The result is " + str(calc.add4(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4]))))
+    print("")
+    sys.exit(0)
 
-if argnumbers != 2 :
+elif argnumbers != 2 or argunumbers != 4 :
     print("")
     print("You entered " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
+    print("Usage: 'add4vals A B C D' where A, B, C and D are individual values.")
+    print("       If add4vals is not in your path, usage is './add4vals A B C D'.")
+    print("       If unbundled, usage is 'python add4vals.py A B C D'.")
     print("")
     sys.exit(1)
