e9bb11372b6ba925e2740d54ac86ff9c1b0af973
denzel-gustave <87156086+denzel-gustave@users.noreply.github.com>
2021-08-19 17:07:46 -0400

    Update test_calc.py
    Added test cases for add4 function
    -Expected fail.

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -43,6 +43,45 @@ class TestCalc(unittest.TestCase):
         """
         result = calc.add2('abc', '5.5')
         self.assertEqual(result, 'abc5.5')
+        
+  #Add 4
+    def test_add4_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.add4(1, 2)
+        self.assertEqual(result, 3)
+
+    def test_add4_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = calc.add4('10.5', 2)
+        self.assertEqual(result, 12.5)
+
+    def test_add4_strings(self):
+        """
+        Test the addition of two strings returns the two strings as one
+        concatenated string
+        """
+        result = calc.add4('abc', 'def')
+        self.assertEqual(result, 'abcdef')
+
+    def test_add4_string_and_integer(self):
+        """
+        Test the addition of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string)
+        """
+        result = calc.add4('abc', 3)
+        self.assertEqual(result, 'abc3')
+
+    def test_add4_string_and_number(self):
+        """
+        Test the addition of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string)
+        """
+        result = calc.add4('abc', '5.5')
+        self.assertEqual(result, 'abc5.5')
 
 if __name__ == '__main__':
     unittest.main()
