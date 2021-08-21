3fc47f566a1899ace4dc4d29931e40f9e50e8828
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-08-21 11:05:22 -0400

    Update add2vals.py

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -8,17 +8,24 @@ import calc
 
 argnumbers = len(sys.argv) - 1
 
-if argnumbers == 2 :
+if argnumbers == 3 :
+    print("")
+    print("The result is " + str(calc.subtract3(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]))))
+    print("")
+    sys.exit(0)
+    
+elif argnumbers == 2:
     print("")
     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
     sys.exit(0)
 
-if argnumbers != 2 :
+elif argnumbers != 3 and argnumbers != 2:
     print("")
     print("You entered " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print("Usage: 'add2vals X Y' where X and Y are individual values. or 'subtract3vals' XYZ where xyz are individual values.")
+    
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
     print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
