418e3b7875893b5ce565329bd0ab049f383d2c2c
denzel-gustave <87156086+denzel-gustave@users.noreply.github.com>
2021-08-21 11:36:45 -0400

    Update test_calc.py
    Updated the add4 method to intake 4 arguments for testing.

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -47,41 +47,41 @@ class TestCalc(unittest.TestCase):
   #Add 4
     def test_add4_integers(self):
         """
-        Test that the addition of two integers returns the correct total
+        Test that the addition of four integers returns the correct total
         """
-        result = calc.add4(1, 2)
-        self.assertEqual(result, 3)
+        result = calc.add4(1, 2, 3, 4)
+        self.assertEqual(result, 10)
 
     def test_add4_floats(self):
         """
-        Test that the addition of two floats returns the correct result
+        Test that the addition of four floats returns the correct result
         """
-        result = calc.add4('10.5', 2)
-        self.assertEqual(result, 12.5)
+        result = calc.add4('10.5', 2, 5, 2.5)
+        self.assertEqual(result, 20.0)
 
     def test_add4_strings(self):
         """
-        Test the addition of two strings returns the two strings as one
+        Test the addition of four strings returns the two strings as one
         concatenated string
         """
-        result = calc.add4('abc', 'def')
-        self.assertEqual(result, 'abcdef')
+        result = calc.add4('abc', 'def', 'ghi', 'jkl')
+        self.assertEqual(result, 'abcdefghijkl')
 
     def test_add4_string_and_integer(self):
         """
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string)
         """
-        result = calc.add4('abc', 3)
-        self.assertEqual(result, 'abc3')
+        result = calc.add4('abc', 3, 'def', 4)
+        self.assertEqual(result, 'abc3def4')
 
     def test_add4_string_and_number(self):
         """
         Test the addition of a string and a float returns them as one
         concatenated string (in which the float is converted to a string)
         """
-        result = calc.add4('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
+        result = calc.add4('abc', '5.5', 'def', 2.5 )
+        self.assertEqual(result, 'abc5.5def2.5')
 
 if __name__ == '__main__':
     unittest.main()
