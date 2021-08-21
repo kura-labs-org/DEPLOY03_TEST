7ab812736444ad183b687debbadd12ee12ec32e1
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-08-21 11:12:10 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -33,17 +33,17 @@ class TestCalc(unittest.TestCase):
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string)
         """
-        result = calc.add2('abc', 3)
-        self.assertEqual(result, 'abc3')
+        result = calc.add2('abc', 10)
+        self.assertEqual(result, 'abc10')
 
     def test_add_string_and_number(self):
         """
         Test the addition of a string and a float returns them as one
         concatenated string (in which the float is converted to a string)
         """
-        result = calc.add2('abc', '5.5')
-        self.assertEqual(result, 'abc5.5')
-        
+        result = calc.add2('abc', '7.5')
+        self.assertEqual(result, 'abc7.5')
+         
     def test_subtract_integers(self):
         """
         Test that the addition of two integers returns the correct total
