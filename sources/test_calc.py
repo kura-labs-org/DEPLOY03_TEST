033b7f5e41756ace197cb1e18da87a63ec1a0db0
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-08-21 11:00:09 -0400

    Update test_calc.py

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -43,6 +43,20 @@ class TestCalc(unittest.TestCase):
         """
         result = calc.add2('abc', '5.5')
         self.assertEqual(result, 'abc5.5')
+        
+    def test_subtract_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.subtract3(1, 2, 3)
+        self.assertEqual(result, -4)
+
+    def test_subtract_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = calc.subtract3('10.5', 2, 3)
+        self.assertEqual(result, 5.5)    
 
 if __name__ == '__main__':
     unittest.main()
